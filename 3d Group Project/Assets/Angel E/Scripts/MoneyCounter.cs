a3b620ff8a43c1e6149dde15e8b08c7e3db38620
Angel Espinoza <62255918+student-ai@users.noreply.github.com>
2025-03-05 10:29:01 -0800

    Pulling my hair out rn
    hlp the new model for the npcs broke my script

---
+++ 3d Group Project/Assets/Angel E/Scripts/MoneyCounter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+
+public class MoneyCounter : MonoBehaviour
+{
+    public static MoneyCounter Instance { get; private set; }
+    public int TotalScore { get; private set; } = 0;
+
+    public static event Action<int> OnScoreUpdated; // Event to update UI
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        TotalScore += points;
+        Debug.Log($"🏆 Points Added: {points} | New Total Score: {TotalScore}");
+
+        OnScoreUpdated?.Invoke(TotalScore); // Notify UI
+    }
+}
